e5860591dbeeb07689779c523a03957527d2890c
crane <jinhesky@hotmail.com>
2019-02-03 21:12:06 +0800

    新增json接口 域名ssl证书文件

--- cmdb/views.py
+++ cmdb/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+import json
 # Create your views here.
 
 
 def index(request):
     return HttpResponse("连接nginx+uwsgi+django………………成功")
+
+
+def interface(request):
+    # return HttpResponse("接口调用成功")
+    resp = {'errorcode': 100, 'detail': 'Get success'}
+    return HttpResponse(json.dumps(resp), content_type="application/json")
