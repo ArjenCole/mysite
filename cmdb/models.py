67fb7b501f44b4c196cd9e1b85122fb0b72156ab
ArjenCole-mpb <jinhesky@hotmail.com>
2019-02-04 11:32:46 +0800

    连接sqlite3

--- cmdb/models.py
+++ cmdb/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+
+
+class cmdb_new (models.Model):    # index是Class的名称与views的相同，也是创建的表名
+    mID = models.IntegerField
+    mNAME = models.TextField(max_length=64)# 创建Char 类型字段，字段长度为64，根据自己实际需求进行更改。
+    mPASSWORD = models.TextField(max_length=64)
--- cmdb/views.py
+++ cmdb/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from cmdb import models   # 引入数据库模型
 import json
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("连接nginx+uwsgi+django………………成功")
+    if request.method == "POST":
+        tname = request.POST.get("tname", None)
+        tpassword = request.POST.get("tpassword", None)
+        models.cmdb_new.objects.create(    # 数据库插入语句
+            # mID=username,   # 设定字段与传入值进行对应（将会什么内容将会保存在什么字段下。）。
+            mNAME=tname,
+            mPASSWORD=tpassword,
+        )
+    user_list = models.cmdb_new.objects.all()     # 将数据全部展示至html中。
+    return render(request, "index.html", {"user_list": user_list})
+    # return render(request, "index.html")
+
+    # return HttpResponse("连接nginx+uwsgi+django………………成功" + request.method)
 
 
 def interface(request):
