6605ff7db6a0bd5b3144b2907d2754ffdf0b6a38
ArjenCole-mpb <jinhesky@hotmail.com>
2019-02-07 23:56:59 +0800

    Revert "创建新的数据库，尝试用小程序操作新数据库未遂"
    This reverts commit d2fcd393

--- cmdb/views.py
+++ cmdb/views.py
@@ -28,24 +28,6 @@ def index(request):
     # return HttpResponse("连接nginx+uwsgi+django………………成功" + request.method)
 
 
-def dbTable(request):
-    '''
-    if request.method == "POST":
-        tname = request.POST.get("tname", None)
-        tpassword = request.POST.get("tpassword", None)
-        models.cmdb_bond_userinfo.objects.create(    # 数据库插入语句
-            # mID=username,   # 设定字段与传入值进行对应（将会什么内容将会保存在什么字段下。）。
-            mNAME=tname,
-            mPASSWORD=tpassword,
-        )
-    '''
-    user_list = models.bond_userinfo.objects.all()
-    # user_list = models.cmdb_new.objects.all()     # 将数据全部展示至html中。
-    return render(request, "dbTable.html", {"user_list": user_list})
-    # return render(request, "dbTable.html")
-
-
-
 def interface(request):
     if request.GET.get("interface", None) is None:
         '''
